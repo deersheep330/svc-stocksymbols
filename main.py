7f86f93693fa2069e8c459249458c33527a6de99
YangChiu <yangchiu@inquartik.com>
2020-12-08 15:46:47 +0800

    close session after query

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from fastapi import FastAPI
 
 engine = create_engine('mysql+pymysql', 'root', 'admin', 'db', '3306', 'mydb')
 #engine = create_engine('postgres', 'admin', 'db', '5432', 'postgres')
-session = start_session(engine)
 
 app = FastAPI()
 
 @app.get("/stocks")
 async def get_stocks_all():
     try:
+        session = start_session(engine)
         res = session.query(Stock).all()
     except Exception as e:
         print(e)
         res = e
-    return res
+    finally:
+        session.close()
+        return res
 
 @app.get("/stocks/{symbol}")
 async def get_stock_by_symbol(symbol: str = None):
     try:
+        session = start_session(engine)
         res = session.query(Stock).filter_by(symbol=symbol).first()
     except Exception as e:
         print(e)
         res = e
-    return res
+    finally:
+        session.close()
+        return res
